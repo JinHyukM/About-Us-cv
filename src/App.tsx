1284fb4fe9d938bb22fb3de5397b89bc1e06818e
JinHyukM <84413953+JinHyukM@users.noreply.github.com>
2021-07-23 17:47:57 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Mission from "./Mission";
+import Image from "./Image";
+import Footer from "./Footer";
+import ExampleComponent from "./components/homepage/ExampleComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="ellipse"></div>
+      <h1 className="heading">About Project Interactivity</h1>
+      <Mission />
+      <Image />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
